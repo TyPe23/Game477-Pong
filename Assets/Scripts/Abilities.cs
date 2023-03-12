47fa4b6c47c2f2b65ab458f5a8d4d8a5c7e66af5
evangray7373@gmail.com <evangray7373@gmail.com>
2023-03-12 16:53:00 -0500

    Ability cooldowns implemented
    Pending actual ability

--- Assets/Scripts/Abilities.cs
+++ Assets/Scripts/Abilities.cs
@@ -16,6 +16,10 @@ public class Abilities : MonoBehaviour
     private Ball Ball;
     public GameObject ball;
 
+    float CooldownTime = 10f;
+    bool isSlowTimeCooldown_P1 = false;
+    bool isSlowTimeCooldown_P2 = false;
+
     void Start()
     {
         // Good code lmao
@@ -41,36 +45,73 @@ public class Abilities : MonoBehaviour
 
     void Update()
     {
+        SetPlayer1ActiveAbilities();
+        SetPlayer2ActiveAbilities();
+        
+        if (Input.GetKeyDown("1") && Player1Abilities[AbilityType.SlowTime] == true)
+        {
+            isSlowTimeCooldown_P1 = true;
+            Player1Abilities[AbilityType.SlowTime] = false;
+            // do thing
+            print("Player 1 slowed down time!");
+            StartCoroutine(P1_SlowTimeCooldown());
+        }
+
+        if (Input.GetKeyDown("8") && Player2Abilities[AbilityType.SlowTime] == true)
+        {
+            isSlowTimeCooldown_P2 = true;
+            Player2Abilities[AbilityType.SlowTime] = false;
+            // do thing
+            print("Player 2 slowed down time!");
+            StartCoroutine(P2_SlowTimeCooldown());
+        }
+    }
+
+    IEnumerator P1_SlowTimeCooldown()
+    {
+        print("Player 1 slow time cooldown begins...");
+        yield return new WaitForSeconds(CooldownTime);
+        print("Player 1 slow time cooldown is over!");
+        isSlowTimeCooldown_P1 = false;
+    }
+
+    IEnumerator P2_SlowTimeCooldown()
+    {
+        print("Player 2 slow time cooldown begins...");
+        yield return new WaitForSeconds(CooldownTime);
+        print("Player 2 slow time cooldown is over!");
+        isSlowTimeCooldown_P2 = false;
+    }
+
+    void SetPlayer1ActiveAbilities()
+    {
         if (Ball.scoreLeft >= 9)
         {
-            Player1Abilities[AbilityType.ShrinkBall] = true;
-            //print("Player 1 has 3 abilities.");
+            //Player1Abilities[AbilityType.ShrinkBall] = true;
         }
         else if (Ball.scoreLeft >= 6)
         {
-            Player1Abilities[AbilityType.FlipControls] = true;
-            //print("Player 1 has 2 abilities.");
+            //Player1Abilities[AbilityType.FlipControls] = true;
         }
-        else if (Ball.scoreLeft >= 3)
+        else if (Ball.scoreLeft >= 3 && !isSlowTimeCooldown_P1)
         {
             Player1Abilities[AbilityType.SlowTime] = true;
-            //print("Player 1 has 1 ability.");
-        }
+        }        
+    }
 
+    void SetPlayer2ActiveAbilities()
+    {
         if (Ball.scoreRight >= 9)
         {
-            Player2Abilities[AbilityType.ShrinkBall] = true;
-            //print("Player 2 has 3 abilities.");
+            //Player2Abilities[AbilityType.ShrinkBall] = true;
         }
         else if (Ball.scoreRight >= 6)
         {
-            Player2Abilities[AbilityType.FlipControls] = true;
-            //print("Player 2 has 2 abilities.");
+            //Player2Abilities[AbilityType.FlipControls] = true;
         }
-        else if (Ball.scoreRight >= 3)
+        else if (Ball.scoreRight >= 3 && !isSlowTimeCooldown_P2)
         {
             Player2Abilities[AbilityType.SlowTime] = true;
-            //print("Player 2 has 1 ability.");
         }
     }
 }
